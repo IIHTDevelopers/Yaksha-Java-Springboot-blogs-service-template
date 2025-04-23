4177dc5936bdcbbdf9fb180d35a09b15c5c05334
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-04-23 14:05:36 +0530

    Update BlogDTO.java

--- src/main/java/com/yaksha/assessments/blogs/dto/BlogDTO.java
+++ src/main/java/com/yaksha/assessments/blogs/dto/BlogDTO.java
@@ -1,32 +1,34 @@
 package com.yaksha.assessments.blogs.dto;
 
 public class BlogDTO {
-	private Long id;
-	private String title;
-	private String content;
 
-	public Long getId() {
-		return id;
-	}
+    private Long id;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    private String title;
 
-	public String getTitle() {
-		return title;
-	}
+    private String content;
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public String getContent() {
-		return content;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
